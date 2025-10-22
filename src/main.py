4d08f7d8dc0042ab88d69a53e85569388a66ff8e
LeoZ213 <113804442+LeoZ213@users.noreply.github.com>
2025-10-22 00:46:41 -0500

    Book-title-and-row-spacing

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import flet as ft
 import tempfile
 import io
+
 from flet.core.file_picker import FilePickerFile
 from flet.core.grid_view import GridView
 from pdf2image import convert_from_path
@@ -39,30 +40,62 @@ def add_to_grid(file: FilePickerFile, book_grid: GridView):
 
     # Get file extension
     file_ext = file.name.rsplit(".", 1)[-1]
+    file_name = file.name.rsplit(".", 1)[0] # Get file name without the extension
     print(file_ext)
+
+    book_cover = None
 
     if file_ext == "pdf":
         # Convert the first page of the PDF to an image
-        book_cover = convert_from_path(file.path, first_page=1, last_page=1)
+        pdf_pages = convert_from_path(file.path, first_page=1, last_page=1)
         # Save as temporary png
         tmp = tempfile.NamedTemporaryFile(suffix = ".png", delete=False)
-        book_cover[0].save(tmp.name, format="PNG")
-        # Add to Grid
-        book_grid.controls.append(ft.Image(src=tmp.name, width=150, height=200))
+        pdf_pages[0].save(tmp.name, format="PNG")
+
+        book_cover = ft.Image(src=tmp.name, width=150, height=200, fit=ft.ImageFit.CONTAIN)
     elif file_ext == "epub":
         epub_cover = get_epub_cover_with_lib(file.path)
+        print(epub_cover)
         if epub_cover:
             tmp = tempfile.NamedTemporaryFile(suffix=".png", delete=False)
             epub_cover.save(tmp.name, format="PNG")
-            book_grid.controls.append(ft.Image(src=tmp.name, width=150, height=200))
+            book_cover = ft.Image(src=tmp.name, width=150, height=200, fit=ft.ImageFit.CONTAIN)
         else:
             # Fallback to icon or placeholder
-            book_grid.controls.append(ft.Icon(ft.Icons.BOOK, size=150))
+            book_cover = ft.Icon(ft.Icons.BOOK, size=150)
     elif file_ext == "txt":
         # Generic Icon for txt placeholder
-        book_grid.controls.append(ft.Icon(ft.Icons.DESCRIPTION, size=150))
+        book_cover = ft.Icon(ft.Icons.DESCRIPTION, size=150)
     else:
         print("Unsupported file type")
+        return
+
+    # Makes sure the icons are in the same size
+    book_cover_container = ft.Container(
+        content=book_cover,
+        width=150,
+        height=200,
+        alignment=ft.alignment.center
+    )
+
+    book_item = ft.Column(
+        controls=[
+            book_cover_container,
+            ft.Text(
+                file_name,
+                size = 12,
+                weight=ft.FontWeight.NORMAL,
+                text_align=ft.TextAlign.CENTER,
+                max_lines=2,
+                overflow=ft.TextOverflow.ELLIPSIS,
+                width=150,
+                color=ft.Colors.BLACK
+            )
+        ],
+        #horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        #alignment=ft.MainAxisAlignment.CENTER
+    )
+    book_grid.controls.append(book_item)
 
     book_grid.update()
 
@@ -113,8 +146,10 @@ def build_book_grid():
         expand=1,
         runs_count=5,
         max_extent=150,
-        child_aspect_ratio=1.0,
-        spacing=5,
+        child_aspect_ratio=0.65,
+        # Change this to increase/decrease vertical space between rows
+        spacing=30,
+        # Change this to increase/decrease horizontal spacing between child
         run_spacing=5,
     )
 
